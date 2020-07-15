f58239834682ab78bdec1c962684f6607894d8bc
devesh chaudhary <deveshn2510@gmail.com>
2020-07-15 17:09:36 +0530

    Updates

---
+++ precache-manifest.fbfde8e58c565ba63bc4841d3a5b1c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2018a673dc6c83f96133fbd6948ce1ca",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "d0f04062120c5fc9d4a6",
+    "url": "/form/static/css/main.78a29e8f.chunk.css"
+  },
+  {
+    "revision": "df67e959aaa81a924f5e",
+    "url": "/form/static/js/2.1b9d5a9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/form/static/js/2.1b9d5a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f04062120c5fc9d4a6",
+    "url": "/form/static/js/main.7f29f387.chunk.js"
+  },
+  {
+    "revision": "bdc12cd774eb02e45c6e",
+    "url": "/form/static/js/runtime-main.f3c56d2f.js"
+  }
+]);
